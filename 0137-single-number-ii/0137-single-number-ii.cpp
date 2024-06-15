f95b8edce114ff494ee09cf4720016f1997333fa
Sameer-Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2024-06-15 23:39:53 +0530

    Time: 12 ms (5.87%), Space: 12.4 MB (25.87%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};

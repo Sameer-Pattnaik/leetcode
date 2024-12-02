8b325dddb0c442e37faa437e54c6e02dde7d29f6
Sameer Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2024-12-03 00:18:37 +0530

    Time: 0 ms (100%), Space: 13.6 MB (61.13%) - LeetHub

---
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) 
+    {
+        int ans = 0;
+        for(auto s:words)
+            if(s.find(pref) == 0) ans += 1;
+        return ans;
+    }
+};

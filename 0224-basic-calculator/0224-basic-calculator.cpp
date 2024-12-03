cae909ae0502f527a2420c0b9f50cb9bdf904c76
Sameer Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2024-12-03 11:41:05 +0530

    Time: 2 ms (61.99%), Space: 10.7 MB (34.59%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int number=0;
+        int sign=1;
+        int res=0;
+        stack<int>eval;
+        for(int i=0;i<s.length();i++)
+        {
+            if(isdigit(s[i]))
+            {
+                number=number*10+(s[i]-'0');
+            }
+            else if(s[i]=='+' || s[i]=='-')
+            {
+                res+=number*sign;
+                if(s[i]=='-')
+                {
+                    sign=-1;
+                }
+                else{
+                    sign=1;
+                }
+                number=0;
+            }
+            else if(s[i]=='(')
+            {
+                eval.push(res);
+                eval.push(sign);
+                res=0;
+                sign=1;
+            }
+            else if(s[i]==')')
+            {
+                res+=number*sign;
+                res*=eval.top();
+                eval.pop();
+                res+=eval.top();
+                eval.pop();
+                number=0;
+            }
+        }
+        return res+number*sign;
+    }
+};

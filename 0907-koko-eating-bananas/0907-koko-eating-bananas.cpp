ecfa9308ea9125c26eeb1af31ce8da4c7ff502d7
Sameer Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2024-12-11 09:42:01 +0530

    Time: 17 ms (18.45%), Space: 39.4 MB (6.86%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPossible(vector<int>piles,int mid,int h)
+    {
+        long long hours=0;
+        for(int i=0;i<piles.size();i++)
+        {
+            hours+=piles[i]/mid;
+            piles[i]%=mid;
+            if(piles[i]>0)
+            {
+                hours++;
+            }
+            if(hours>h)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+       int maxi=*max_element(piles.begin(),piles.end());
+       int ans=0;
+       
+       int mini=1;
+       while(mini<=maxi)
+       {
+        int mid=(mini+maxi)/2;
+        if(isPossible(piles,mid,h))
+        {
+            ans=mid;
+            maxi=mid-1;
+        }
+        else{
+            mini=mid+1;
+        }
+       }
+       return ans; 
+
+    }
+};

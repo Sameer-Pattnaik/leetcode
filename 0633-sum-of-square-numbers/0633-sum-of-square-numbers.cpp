6dfe784beaf294dab4873dc5371486330a3996a2
Sameer Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2025-04-02 23:13:55 +0530

    Time: 0 ms (100%), Space: 7.6 MB (87.42%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,14 @@
+#define ll long long
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        ll left = 0, right = static_cast<ll>(sqrt(c));
+        while(left <= right) {
+            if(left * left + right * right == c) return true;
+            else if(left * left + right * right > c) right--;
+            else left++;
+        }
+
+        return false;
+    }
+};

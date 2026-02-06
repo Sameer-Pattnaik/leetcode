0ad0366c173336f8f29d8ff303907c3f78062bc2
Sameer Pattnaik <163635718+Sameer-Pattnaik@users.noreply.github.com>
2026-02-06 11:36:03 +0530

    Time: 35 ms (48.84%), Space: 105.1 MB (52.91%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 0;
+        int maxLen = 0;
+        
+        for (int j = 0; j < n; j++) {
+            // Cast to long long to prevent overflow
+            while ((long long)nums[j] > (long long)nums[i] * k) {
+                i++;
+            }
+            maxLen = max(maxLen, j - i + 1);
+        }
+        
+        return n - maxLen;
+    }
+};
